8e1e1ffb411aae8c43b3826b841e97f1fed60a3f
Punkes <119493977+punkes@users.noreply.github.com>
2024-02-27 14:25:14 +0100

    Update and rename owncloud-autoinstall-v1.0.sh to owncloud-autoinstall-v1.2.sh

--- owncloud-autoinstall-v1.2.sh
+++ owncloud-autoinstall-v1.2.sh
@@ -26,6 +26,18 @@ if [ "$is_root" != "yes" ]; then
   exit
 fi
 
+# Demander le nom de l'utilisateur
+read -p "Entrez le nom de l'utilisateur à passer en sudo/root : " username
+
+# Vérifier si l'utilisateur existe
+if ! id "$username" &>/dev/null; then
+    echo "L'utilisateur $username n'existe pas"
+    exit 1
+fi
+
+# Ajouter l'utilisateur au groupe sudo s'il n'y est pas déjà
+usermod -aG sudo "$username"
+
 # Prompt user for MySQL username, password, and database name
 read -p "Enter MySQL username: " mysql_user
 read -sp "Enter MySQL password: " mysql_password
@@ -33,25 +45,28 @@ echo
 read -p "Enter MySQL database name: " mysql_database
 
 # Ensure the system package cache is up to date
-apt update
+apt update -y && apt upgrade -y
+
+# Install sudo
+apt install sudo
 
 # Install the Apache web server
-apt install apache2
+apt install apache2 -y
 
 # Ensure Apache2 service is started
 systemctl enable --now apache2
 
 # Ensure the system package cache is up to date
-apt update
+apt update -y
 
 # Install MariaDB 10
-apt install mariadb-server
+apt install mariadb-server -y
 
 # Ensure MariaDB is running
 systemctl enable --now mariadb
 
 # Install the SURY APT repository
-apt update
+apt update -y
 apt -y install apt-transport-https lsb-release ca-certificates curl wget gnupg2
 
 wget -qO- https://packages.sury.org/php/apt.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/sury-php.gpg
@@ -59,10 +74,10 @@ wget -qO- https://packages.sury.org/php/apt.gpg | gpg --dearmor > /etc/apt/trust
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
 
 # Ensure the system package cache is up to date
-apt update
+apt update -y
 
 # Install PHP 7.x
-apt install php7.4
+apt install php7.4 -y
 
 # Install additional PHP modules
 apt install libapache2-mod-php7.4 php7.4-{mysql,intl,curl,gd,xml,mbstring,zip} -y
@@ -70,7 +85,7 @@ apt install libapache2-mod-php7.4 php7.4-{mysql,intl,curl,gd,xml,mbstring,zip} -
 # Install the ownCloud repository
 echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/server:/10/Debian_12/ /' | tee /etc/apt/sources.list.d/isv:ownCloud:server:10.list
 curl -fsSL https://download.opensuse.org/repositories/isv:ownCloud:server:10/Debian_12/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/isv_ownCloud_server_10.gpg > /dev/null
-apt update
+apt update -y
 apt install owncloud-complete-files -y
 
 # Configure Apache for ownCloud
